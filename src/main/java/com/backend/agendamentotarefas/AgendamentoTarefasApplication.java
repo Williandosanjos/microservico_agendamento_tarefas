68e23cbe370bf05c8c3eed82295647e609d44a60
Willian dos Anjos Boaventura <williandosanjos@gmail.com>
2026-03-16 10:04:53 -0300

    commit com estrutura inicial do microserviço agendador de tarefas

---
+++ src/main/java/com/backend/agendamentotarefas/AgendamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.backend.agendamentotarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/agendamentotarefas/AgendamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.agendamentotarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
