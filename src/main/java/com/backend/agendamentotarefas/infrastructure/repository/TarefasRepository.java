a64f5daaade3dc401816e92c4eaddaa5a5ee2b3b
Willian dos Anjos Boaventura <williandosanjos@gmail.com>
2026-03-24 12:25:08 -0300

    Implementação de método GET de consulta por periodo e por email

--- src/main/java/com/backend/agendamentotarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/backend/agendamentotarefas/infrastructure/repository/TarefasRepository.java
@@ -1,9 +1,17 @@
 package com.backend.agendamentotarefas.infrastructure.repository;
 
+import com.backend.agendamentotarefas.business.dto.TarefasDTO;
 import com.backend.agendamentotarefas.infrastructure.entity.TarefasEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
+
+    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+
+    List<TarefasEntity> findByEmailUsuario(String email);
 }
